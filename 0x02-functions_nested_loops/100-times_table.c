dd361673aba564b8c5e2b72a9e3f811ec642dcf7
Ndushie <clareg182@gmail.com>
2023-04-25 14:58:53 -0700

    TASK100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -33,7 +33,7 @@ void print_times_table(int n)
 				{
 					_putchar((product / 10) + '0');
 				}
-				putchar((product % 10) = '0');
+				_putchar((product % 10) + '0');
 			}
 			_putchar('\n');
 		}
