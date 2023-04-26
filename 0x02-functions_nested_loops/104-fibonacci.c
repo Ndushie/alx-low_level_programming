507fa7834e1566dfc93d5d8be6915c07229dfbad
Ndushie <clareg182@gmail.com>
2023-04-25 18:31:11 -0700

    TASK104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
  *
@@ -7,12 +8,11 @@
 int main(void)
 {
 	int count;
-	unsigned long fib1 = 1, fib2 = 2, sum;
+	unsigned long fib1 = 0, fib2 = 1, sum;
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
 
-	printf("%lu, %lu, ", fib1, fib2);
-	for (count = 0; count < 90; count++)
+	for (count = 0; count < 92; count++)
 	{
 		sum = fib1 + fib2;
 		printf("%lu, ", sum);
@@ -23,17 +23,17 @@ int main(void)
 	fib2_half1 = fib2 / 10000000000;
 	fib1_half2 = fib1 % 10000000000;
 	fib2_half2 = fib2 % 10000000000;
-	for (count = 92; count < 100; count++)
+	for (count = 93; count < 99; count++)
 	{
 		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 + fib2_half2;
-		if (half2 > 9999999999)
+		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
 			half1 += 1;
 			half2 %= 1000000000;
 		}
 		printf("%lu%010lu", half1, half2);
-		if (count != 99)
+		if (count != 98)
 		{
 			printf(", ");
 		}
