2585e476cf8582f7475bb5761c8770b61eb76df8
Ndushie <clareg182@gmail.com>
2023-04-25 17:36:14 -0700

    TASK104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -34,7 +34,9 @@ int main(void)
 		}
 		printf("%lu%010lu", half1, half2);
 		if (count != 97)
+		{
 			printf(", ");
+		}
 			fib1_half1 = fib2_half1;
 			fib1_half2 = fib2_half2;
 			fib2_half1 = half1;
