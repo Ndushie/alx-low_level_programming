1b6b79868be773fedf22f4fd646ac2c85480791d
Ndushie <clareg182@gmail.com>
2023-04-27 07:51:41 -0700

    TASK9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
  *
@@ -12,19 +13,23 @@ int main(void)
 	{
 		if (a % 3 == 0 && a % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (a % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (a % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", a);
+			printf("%d", a);
+		}
+		if (a != 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
