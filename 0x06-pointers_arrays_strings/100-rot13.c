a142c499cf64069ae6761e3a2c0369358168cb81
Ndushie <clareg182@gmail.com>
2023-05-04 07:09:52 -0700

    ROT13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - replaces a letter with the 13th letter after it.
+ *
+ * @string: the string to be encoded.
+ *
+ * Return: pointer to the resulting encoded string.
+ */
+char *rot13(char *string)
+{
+	int x;
+	int y;
+	char alphabet1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char alphabet2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (x = 0; string[x] != '\0'; x++)
+	{
+		for (y = 0; y < 52; y++)
+		{
+			if (string[x] == alphabet1[x])
+			{
+				string[x] = alphabet2[y];
+				break;
+			}
+		}
+	}
+	return (string);
+}
