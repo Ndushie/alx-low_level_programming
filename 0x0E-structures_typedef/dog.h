58edcf19cf654ad40e422f7845f3405e0ce1334c
Ndushie <clareg182@gmail.com>
2023-05-22 01:08:49 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -23,7 +23,9 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 dog_t *create_dog(char *name, float age, char *owner);
+
 void free_dog(dog_t *d);
+
 void print_dog(dog_t *d);
 
 #endif /* DOG_H */
